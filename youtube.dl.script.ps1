35c2872ddad113cf58f327679d778cf1a37d1ef7
xutto <neo_xutto@hotmail.com>
2022-02-07 23:32:49 +0100

    update

--- youtube.dl.script.ps1
+++ youtube.dl.script.ps1
@@ -1,4 +1,12 @@
 Write-Host "===== download mp3 by link ===== ";
 $linkToYoutube = Read-Host -Prompt "plase paste youtube link to download";
 Write-Host "======  Download -> " $linkToYoutube;
-youtube-dl --format best -x --audio-format mp3 --audio-quality 320 --prefer-ffmpe $linkToYoutube
+.\youtube-dl.exe --format best -x --audio-format mp3 --audio-quality 320 --prefer-ffmpe -o "D:\Download\music\%(title)s.%(ext)s" $linkToYoutube
+
+
+# Descargar más de un vídeo de un listado
+#
+#-a, --batch-file FILE            File containing URLs to download ('-' for
+#                                 stdin), one URL per line. Lines starting
+#                                 with '#', ';' or ']' are considered as
+#                                 comments and ignored.
